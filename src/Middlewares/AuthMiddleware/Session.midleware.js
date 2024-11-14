f4f607e1d4a9d1ca47a79b5d6ee6e03e81c2608a
Vipul sharma <vipulsharma6021@gmail.com>
2024-11-15 00:43:17 +0530

    Use session file as midleware

--- src/Middlewares/AuthMiddleware/Session.midleware.js
+++ src/Middlewares/AuthMiddleware/Session.midleware.js
@@ -1,21 +1,19 @@
-// import { ApiError } from "../../utils/ApiError.js";
-// import { db } from "../../db/server.db.js";
+import { ApiError } from "../../utils/ApiError.js";
+import { db } from "../../db/server.db.js";
 
-// const { User } = db;
+const { User } = db;
 
-// const setSessionForUser = async (userId) => {
-//     const user = await User.findByPk(userId);
+const setSessionForUser = async (req, userId) => {
+    const user = await User.findByPk(userId);
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+    req.session.userId = user.userid;
+    req.session.email = user.email;
+    req.session.role = user.role;
+    await req.session.save();
 
-//         if (!user) {
-//             throw new ApiError(404, "User not found");
-//         }
-//         req.session.userId = user.id;
-//         req.session.email = user.email;
-//         req.session.role = user.role;
+    return { message: "Session created successfully" };
+};
 
-//         await req.session.save();
-
-//         return { message: "Session created successfully" };
-// };
-
-// export { setSessionForUser };
+export { setSessionForUser };
