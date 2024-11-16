33c60260637096733448a88072a62e7036347b1c
Vipul sharma <vipulsharma6021@gmail.com>
2024-11-16 14:03:23 +0530

    improve Auth middleware

--- src/Middlewares/AuthMiddleware/auth.middlewares.js
+++ src/Middlewares/AuthMiddleware/auth.middlewares.js
@@ -1,24 +1,23 @@
 import { ApiError } from '../../utils/ApiError.js';
-import { asyncHandler } from '../../utils/asyncHandler.js'; 
+import { asyncHandler } from '../../utils/asyncHandler.js';
 import { db } from '../../db/server.db.js';
 
 const { User } = db;
 
 const verifySession = asyncHandler(async (req, _, next) => {
     if (!req.session || !req.session.userId) {
-        return next(new ApiError(401, "Not authenticated. Please log in."));
+        return next(new ApiError(401, "Session not valid, please log in again."));
     }
 
     try {
         const user = await User.findOne({
-            where: { userid: req.session.userid },
+            where: { userid: req.session.userId },
             attributes: { exclude: ['otp', 'refreshToken'] },
         });
 
         if (!user) {
             return next(new ApiError(401, "Session is invalid. Please log in again."));
         }
-
         req.user = user;
         next();
     } catch (error) {
